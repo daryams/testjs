26a9bfe639bbed310b65a18e8687f4428742d50e
darya <darya.shkuratova@gmail.com>
2020-06-06 01:15:29 +1000

    types of data

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let money = 102000;
+let money = 120000;
 let income = 'freelance';
 let addExpenses = 'интернет, такси, квартира';
 let deposit = true;
-let misson = 1000000;
+let mission = 1000000;
 let period = 12;
+let budgetDay = money/30;
 
 console.log('Master test');
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase());
+
+let expenses = addExpenses.split(', ');
+console.log(expenses[1]);
+
+console.log(budgetDay);
+
+
+

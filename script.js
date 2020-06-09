3787364717e7292cccf0528d66fd3d38711c2232
darya <darya.shkuratova@gmail.com>
2020-06-09 22:38:02 +1000

    hardlesson3

--- script.js
+++ script.js
@@ -5,4 +5,25 @@ let deposit;
 let misson;
 let period;
 
-console.log('Master test');
+console.log('Hardlesson 3');
+
+let lang = prompt('Укажите язык', 'ru');
+console.log("Вы выбрали язык: ");
+
+switch (lang) {
+    case 'ru':
+        console.log('Русский');
+        break;
+    case 'en':
+        console.log('English');
+        break;
+    default:
+        console.log('Хз чо за язык');
+        break;
+}
+
+if (lang == 'ru') {
+    console.log('Пн Вт Ср Чт Пт Сб Вс');
+} else if ( lang == 'en') {
+    console.log('Mn Tu Wd Th Fr St Su');
+}

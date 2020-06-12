27806204b355790c600f03a979f639428ce89fd6
darya <darya.shkuratova@gmail.com>
2020-06-12 17:29:35 +1000

    ready

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-let money, income, addExpenses, deposit, mission, period, budgetDay;
+let money, income, addExpenses, deposit, mission, period, budgetDay, expensesMonth;
 
 function start() {
     do {
@@ -15,32 +15,35 @@ function start() {
 start();
 console.log('Месячный доход: ', money);
 
-addExpenses = prompt('Возможные расходы через запятую:', 'sex, drugs, rocknroll');
-deposit = confirm('А депозит есть?');
+// addExpenses = prompt('Возможные расходы через запятую:', 'sex, drugs, rocknroll');
+// deposit = confirm('А депозит есть?');
 
 let expensesQuest1 = 'Введи обязательную статью расхода:';
 let expensesQuest2 = 'Во сколько это обойдется?';
 
-
 function getExpensesMonth () {
-    let sum, i = 0;
+    let sum = 0;
     let expenses = [];
+    let expensesText = [];
 
     for (let i = 0; i < 2; i++) {
-        prompt(expensesQuest1);
+        expensesText[i] = prompt(expensesQuest1);
         
         do {  
             expenses[i] = prompt(expensesQuest2);
         } while (!isNumber(expenses[i]));
         
-        sum += Number(expenses[i]);
+        sum += +expenses[i];
+        
     }        
+
     return sum;
     
 }
-
-let expensesMonth = getExpensesMonth();
+expensesMonth = getExpensesMonth();
 console.log('Обязательные расходы: ' + expensesMonth);
+
+
 
 function getAccumulatedMonth(inc, exp) {
     let result = Number(inc) - Number(exp);
@@ -63,21 +66,21 @@ if (targetMonth > 0) {
 
 
 
-budgetDay = Math.floor(accumulatedMonth/30);
-console.log('Бюджет на день: ', budgetDay);
+// budgetDay = Math.floor(accumulatedMonth/30);
+// console.log('Бюджет на день: ', budgetDay);
 
 
-function getStatusIncome(b) {
-    if (b > 1200 || b == 1200) {
-        console.log('Слишком много денег!');
-    } else if (b > 600 && b < 1200) {
-        console.log('Средненько -_-');
-    } else if ((b < 600 && b > 0) || b == 600) {
-        console.log('Нищеброд ебаный!');
-    } else if (b < 0 || b == 0) {
-        console.log('Это пиздец');
-    }    
-}
+// function getStatusIncome(b) {
+//     if (b > 1200 || b == 1200) {
+//         console.log('Слишком много денег!');
+//     } else if (b > 600 && b < 1200) {
+//         console.log('Средненько -_-');
+//     } else if ((b < 600 && b > 0) || b == 600) {
+//         console.log('Нищеброд ебаный!');
+//     } else if (b < 0 || b == 0) {
+//         console.log('Это пиздец');
+//     }    
+// }
 
-getStatusIncome(budgetDay);
+// getStatusIncome(budgetDay);
 

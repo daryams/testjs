58b9a42a30c6f26d09bc701630a1a3ecd2fea594
darya <darya.shkuratova@gmail.com>
2020-06-15 20:37:15 +1000

    hardlesson7

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 'use sctrict';
 
+const week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+const now = new Date();
+console.log( now.getDay() );
+
+let dayCount = 0;
+
+for (const day in week) {
+    dayCount++;
+    if (dayCount == now.getDay()) {
+        document.write('<p><b style="color: green;">'+ week[day] +'</b></p>'); 
+    }
+    else if (dayCount == 6 || dayCount == 7 ) {
+        document.write('<p><i style="color: red;">'+ week[day] +'</i></p>'); 
+    } else {
+        document.write('<p>'+ week[day] +'</p>'); 
+    }
+
+    
+       
+}
